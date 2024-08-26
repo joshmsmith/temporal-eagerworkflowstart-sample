a9aa756a9d368f9fca6e38d0c8aad6126d3f5e4d
Joshua Smith <joshmsmith@gmail.com>
2024-08-26 10:44:11 -0400

    fixing tests

--- src/test/java/eagerworkflowstart/EagerWorkflowTest.java
+++ src/test/java/eagerworkflowstart/EagerWorkflowTest.java
@@ -1,23 +1,16 @@
 package eagerworkflowstart;
 
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
 import io.temporal.testing.TestWorkflowEnvironment;
 import io.temporal.worker.Worker;
 
+import static org.mockito.Mockito.mock;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
-import eagerworkflowstart.HelloActivities;
-import eagerworkflowstart.HelloActivitiesImpl;
-import eagerworkflowstart.IntroductionWorkflow;
-import eagerworkflowstart.IntroductionWorkflowImpl;
-import eagerworkflowstart.Shared;
 
 public class EagerWorkflowTest {
 
@@ -27,28 +20,30 @@ public class EagerWorkflowTest {
 
     @Before
     public void setUp() {
-        testEnv = TestWorkflowEnvironment.newInstance();
-        worker = testEnv.newWorker(Shared.EAGER_TASK_QUEUE);
-        worker.registerWorkflowImplementationTypes(IntroductionWorkflowImpl.class);
-        workflowClient = testEnv.getWorkflowClient();
     }
 
-    @After
-    public void tearDown() {
-        testEnv.close();
-    }
 
     @Test
-    public void testTransfer() {
-        HelloActivities activities = mock(HelloActivitiesImpl.class);
-        worker.registerActivitiesImplementations(activities);
+    public void testGreeting() {
+        
+        testEnv = TestWorkflowEnvironment.newInstance();
+        worker = testEnv.newWorker(Shared.EAGER_TASK_QUEUE);
+        worker.registerWorkflowImplementationTypes(GreetingWorkflowImpl.class);
+        worker.registerActivitiesImplementations(new HelloActivitiesImpl());
+        workflowClient = testEnv.getWorkflowClient();
         testEnv.start();
         WorkflowOptions options = WorkflowOptions.newBuilder()
                 .setTaskQueue(Shared.EAGER_TASK_QUEUE)
                 .build();
-        IntroductionWorkflow workflow = workflowClient.newWorkflowStub(IntroductionWorkflowImpl.class, options);
-        workflow.getIntroduction("Bob", "fooseball");
+
+        GreetingWorkflow workflow = workflowClient.newWorkflowStub(GreetingWorkflow.class, options);
         
-        //verify(activities).deposit(eq("account2"), eq("reference1"), eq(1.23));
+        workflow.getGreeting("Bob");
+        
+    }
+    
+    @After
+    public void tearDown() {
+        testEnv.close();
     }
 }
